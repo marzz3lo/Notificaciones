b7c035cf7d5ac556de64ec901f0479858aa2d83a
marzz3lo <marzzelo@gmail.com>
2017-06-20 19:53:35 -0400

    Ejercicio: Una aplicación Android con notificaciones para Wear

--- app/src/main/java/moonlapse/com/notificaciones/MainActivity.java
+++ app/src/main/java/moonlapse/com/notificaciones/MainActivity.java
@@ -1,13 +1,35 @@
 package moonlapse.com.notificaciones;
 
+import android.app.Notification;
+import android.support.v4.app.NotificationManagerCompat;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.app.NotificationCompat;
+import android.text.Html;
+import android.view.View;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+
         setContentView(R.layout.activity_main);
+
+        Button wearButton = (Button) findViewById(R.id.boton1);
+        wearButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                int notificacionId = 001;
+                Notification notificacion = new NotificationCompat.Builder(MainActivity.this)
+                        .setSmallIcon(R.mipmap.ic_launcher)
+                        .setContentTitle("Título")
+                        .setContentText(Html.fromHtml("<b>Notificación</b> <u>Android Wear</u>"))
+                        .build();
+                NotificationManagerCompat notificationManager = NotificationManagerCompat.from(MainActivity.this);
+                notificationManager.notify(notificacionId, notificacion);
+            }
+        });
     }
 }
